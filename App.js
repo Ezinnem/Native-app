89a852b9c63bb4e2a17eb4f355404dcaad57697e
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2021-07-02 22:44:08 +0100

    Created a add item page

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  const [ OutputText, setOutputText ] = useState ('This is the text and it is beautiful')
-  return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Life Goal" style={{borderBottomColor: "black", borderWidth: 1, padding: 10, width: "70%"}}/>
-        <Button title="Add item"/>
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [lifeGoals, setLifeGoals] = useState([]);
 
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setLifeGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Set a Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal}/>
+        <Button title="Add item" onPress={addGoalHandler}/>
       </View>
+      < FlatList keyExtractor={(item, index) => item.id} data={lifeGoals} renderItem={itemData => (
+        <View style={styles.goalList}>
+        <Text>{itemData.item.value}</Text>
+        </View>
+      )} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "70%"
+  },
+  goalList: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: "#efefef",
+    borderWidth: 1,
+    marginVertical: 10,
+  }
 });
